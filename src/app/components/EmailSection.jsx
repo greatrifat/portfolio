07565ac7debf1228a9a9ce3fcf7fa692d0f9eb6a
greatrifat <greatrifat@gmail.com>
2024-11-18 03:31:55 +0600

    minor 1

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -47,7 +47,7 @@ const EmailSection = () => {
       className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
     >
       
-     
+      <Script src="https://platform.linkedin.com/badges/js/profile.js" />
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
@@ -59,7 +59,14 @@ const EmailSection = () => {
           open. Whether you have a question or just want to say hi, I&apos;ll
           try my best to get back to you!
         </p>
-        
+        <div className="socials flex flex-row gap-2">
+          <Link href="https://github.com/greatrifat">
+            <Image src={GithubIcon} alt="Github Icon" />
+          </Link>
+          <Link href="https://www.linkedin.com/in/greatrifat">
+            <Image src={LinkedinIcon} alt="Linkedin Icon" />
+          </Link>
+        </div>
         
         <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="greatrifat" data-version="v1"><a className="badge-base__link LI-simple-link" href="https://bd.linkedin.com/in/greatrifat?trk=profile-badge">Md Robayet Ahasan Rifat</a></div>
     
